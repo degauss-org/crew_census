7d7f0d66c22e4e2e5ae0bcd125b05d5736259621
Cole Brokamp <cole.brokamp@gmail.com>
2018-09-26 13:39:46 -0400

    update for inflation fixes and new ARiC deprivation index

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 [![Docker Repository on Quay](https://quay.io/repository/degauss/crew_census/status?token=7eaa4f26-4a6c-498f-86d9-dd0e68be5fc4 "Docker Repository on Quay")](https://quay.io/repository/degauss/crew_census)
 > Historical census tracts and data DeGAUSS container built for CREW.
 
+## Versions
+
+| release tag | date    | notes                                                     |
+|-------------|---------|-----------------------------------------------------------|
+| 0.1         | 2/24/18 | initial release                                           |
+| 0.2         | 9/26/18 | update for inflation fixes and new ARiC deprivation index |
 
 ## Private Images
 
@@ -11,7 +17,7 @@ The container is distributed privately and to gain access, you will need to:
 1. Create an account at [quay.io](https://quay.io)
 2. Email your quay.io username to cole dot brokamp at cchmc dot org to be added to access list
 3. Login at the Docker command line (`docker login quay.io`); you will be prompted for your username and password
-4. Pull the image with `docker pull quay.io/degauss/crew_census:0.1`
+4. Pull the image with `docker pull quay.io/degauss/crew_census:0.2`
 
 ## Using
 
@@ -22,7 +28,7 @@ DeGAUSS arguments specific to this container:
 
 Example call:
 
-`docker run --rm=TRUE -v $PWD:/tmp quay.io/degauss/crew_census:0.1 geocoded_csv_file.csv 2000`
+`docker run --rm=TRUE -v $PWD:/tmp quay.io/degauss/crew_census:0.2 geocoded_csv_file.csv 2000`
 
 In the above example call, replace `geocoded_csv_file.csv` with the name of your geocoded csv file and `2000` with the decentennial census year to be used for the tract overlay and merging of census data.
 
--- make_crew_geodata.R
+++ make_crew_geodata.R
@@ -7,15 +7,18 @@ library(sf)
 
 # d_1980 <- read_csv('crew_tract_data/CREW_Tract_1980.csv')
 d_1990 <- read_csv('crew_tract_data/CREW_Tract_1990.csv')
-d_2000 <- read_csv('crew_tract_data/crew_tract_2000.csv')
+d_2000 <- read.csv('crew_tract_data/crew_tract_2000.csv', stringsAsFactors = FALSE) %>%
+  as_tibble() %>%
+  mutate(Geo_FIPS_2000 = as.character(Geo_FIPS_2000)) %>%
+  mutate(Geo_FIPS_2000 = stringr::str_pad(Geo_FIPS_2000, 11, pad = '0'))
 d_2010 <- read.csv('crew_tract_data/crew_tract_2010.csv', stringsAsFactors = FALSE) %>%
-    as_tibble() %>%
-    mutate(Geo_FIPS_2010 = as.character(Geo_FIPS_2010)) %>%
-    mutate(Geo_FIPS_2010 = stringr::str_pad(Geo_FIPS_2010, 11, pad = '0'))
+  as_tibble() %>%
+  mutate(Geo_FIPS_2010 = as.character(Geo_FIPS_2010)) %>%
+  mutate(Geo_FIPS_2010 = stringr::str_pad(Geo_FIPS_2010, 11, pad = '0'))
 
-# s_1980 <- read_sf('tract_shps/US_tract_1980_conflated.shp')
-s_1990 <- read_sf('tract_shps/us_tract_1990_conflated.shp')
-s_2000 <- read_sf('tract_shps/US_tract_2000_conflated.shp')
+# s_1980 <- read_sf('crew_tract_shps/US_tract_1980_conflated.shp')
+s_1990 <- read_sf('crew_tract_shps/us_tract_1990_conflated.shp')
+s_2000 <- read_sf('crew_tract_shps/US_tract_2000_conflated.shp')
     # quickfix: for 3 tracts with missing state/county
     # "5221" should have (state/county) 36059 (NY)
     # "2011" should have 36103 (NY)
@@ -25,7 +28,7 @@ s_2000[is.na(s_2000$STATE) & s_2000$TRACT == 2011, 'COUNTY'] <- '103'
 s_2000[is.na(s_2000$STATE) & s_2000$TRACT == 2014, 'COUNTY'] <- '103'
 s_2000[is.na(s_2000$STATE), 'STATE'] <- '36'
 
-s_2010 <- read_sf('tract_shps/us_tract_2010.shp')
+s_2010 <- read_sf('crew_tract_shps/us_tract_2010.shp')
 
 # s_1980 %<>%
 #     as_tibble() %>%
@@ -49,7 +52,7 @@ s_2000 %<>%
     mutate(TRACT = stringr::str_pad(TRACT, 6, pad = '0')) %>%
     transmute(tract_fips = paste0(STATE, COUNTY, TRACT)) %>%
     left_join(d_2000, by = c('tract_fips' = 'Geo_FIPS_2000')) %>%
-    st_transform(5072)
+  st_transform(5072)
 
 s_2010 %<>%
     as_tibble() %>%
